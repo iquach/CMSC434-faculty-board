3b79df2c8dff95fbfe7d83e55b097055aa7fca58
Vikram Yabannavar <vyaban@gmail.com>
2018-03-25 19:48:15 -0400

    12 hour time and am/pm

--- js/time.js
+++ js/time.js
@@ -3,16 +3,18 @@ function checkTime(i) {
 	    i = "0" + i;
 	  }
 	  return i;
-	}
-	function startTime() {
+}
+
+function startTime() {
 	  var today = new Date();
 	  var h = today.getHours();
 	  var m = today.getMinutes();
-	  // add a zero in front of numbers <10
+	  var ampm = h < 12 ? " AM" : " PM";
 	  m = checkTime(m);
-	  document.getElementById('time').innerHTML = h + ":" + m;
+	  document.getElementById('time').innerHTML = (h%12) + ":" + m + ampm;
 	  t = setTimeout(function() {
 	    startTime()
 	  }, 500);
-	}
+}
+
 startTime();
